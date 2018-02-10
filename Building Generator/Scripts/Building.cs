e3410c61dfb6fb670c2dff9b0fa4c1ef2459a98d
ElSergio217 <sergioa.garcia217@gmail.com>
2018-02-09 20:11:59 -0500

    v1.0

---
+++ Building Generator/Scripts/Building.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour {
+
+    public GameObject Ledge; 
+
+	void Start () {
+        gen();
+	}
+
+    void gen()
+    {
+        int count = this.transform.childCount;
+
+        for (int i = 0; i < count; ++i)
+        {
+            Vector3 scale = this.transform.GetChild(i).transform.localScale;
+            this.transform.GetChild(i).transform.localScale = new Vector3(scale.x + Random.Range(1.25f, 2.0f), Random.Range(1.0f, 15.0f), Random.Range(1.25f, 2.0f) + scale.z);
+            GameObject led = Instantiate(Ledge);
+            led.transform.parent = this.transform.GetChild(i).transform;
+            led.transform.localRotation = this.transform.GetChild(i).transform.localRotation;
+            led.transform.localPosition = new Vector3(0, 1, 0);
+            led.transform.localScale = new Vector3(1, 1.5f/transform.GetChild(i).transform.localScale.y, 1);
+        }
+    }
+}
---
+++ Building Generator/Scripts/Generator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour {
+
+    public GameObject[] seed;
+    public float scale = 1;
+
+
+	// Use this for initialization
+	void Start () {
+        Generate();
+	}
+	
+	// Update is called once per frame
+	void Generate () {
+        GameObject Building = Instantiate(seed[Random.Range(0, seed.Length)], transform.position, transform.rotation);
+        Building.transform.localScale = new Vector3(scale, scale, scale);
+	}
+}
